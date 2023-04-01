c1d686d9f2ab6ab642274b33e6224f96032bda2d
Aman <amanrathod8888@gmail.com>
2023-04-01 21:32:43 +0530

    Test Commit
    Added admin url

--- src/TestUI.java
+++ src/TestUI.java
@@ -6,7 +6,7 @@ public class TestUI {
     public static void main(String[] args) throws InterruptedException {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
             System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
             WebDriver x = new ChromeDriver();
@@ -16,6 +16,8 @@ public class TestUI {
             x.findElement(By.xpath("//input[@id='username']")).sendKeys("lee@motadata.com");
             x.findElement(By.xpath("//input[@id='password']")).sendKeys("admin@123");
             x.findElement(By.xpath("//button[@id='login-btn']")).click();
+            Thread.sleep(3000);
+            x.get("https://sky.serviceops.ai/admin");
 
 
         }
