badda9feaaf0f8d8bffdc651ef54e0f11f1b4749
Aman <amanrathod8888@gmail.com>
2023-04-14 17:56:13 +0530

    - xPath class with all xpath - Admin href xpath added

--- src/TestUI.java
+++ src/TestUI.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 // This is Test 0
-public class TestUI {
+public class TestUI extends xPath {
     public static void main(String[] args) throws InterruptedException {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
@@ -11,13 +14,15 @@ public class TestUI {
             System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
             WebDriver x = new ChromeDriver();
             x.manage().window().maximize();
+            x.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
             x.get("https://sky.serviceops.ai/login");
-            Thread.sleep(2000);
-            x.findElement(By.xpath("//input[@id='username']")).sendKeys("lee@motadata.com");
-            x.findElement(By.xpath("//input[@id='password']")).sendKeys("admin@123");
-            x.findElement(By.xpath("//button[@id='login-btn']")).click();
-            Thread.sleep(3000);
-            x.get("https://sky.serviceops.ai/admin");
+            //Thread.sleep(2000);
+            x.findElement(By.xpath(xPath.username)).sendKeys("lee@motadata.com");
+            x.findElement(By.xpath(xPath.password)).sendKeys("admin@123");
+            x.findElement(By.xpath(xPath.loginbutton)).click();
+           // Thread.sleep(3000);
+            x.findElement(By.xpath(xPath.admin)).click();
+            x.navigate().refresh();
 
 
 
---
+++ src/xPath.java
@@ -0,0 +1,6 @@
+public class xPath {
+    static String username = "//input[@id='username']";
+    static String password = "//input[@id='password']";
+    static String loginbutton = "//button[@id='login-btn']";
+    static String admin = "//a[@href='/admin/']";
+}
